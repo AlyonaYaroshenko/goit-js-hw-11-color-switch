f1bc979b641638e894198e957d6aba98a535596b
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-05-23 17:16:01 +0300

    finished

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"'),
+  stopBtn: document.querySelector('button[data-action="stop"'),
+};
+let timerId = undefined    
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+refs.startBtn.addEventListener('click', setRandomColor);
+refs.stopBtn.addEventListener('click', stopInterval);
+
+function setRandomColor() {
+  console.log(document.body.style.backgroundColor);
+    document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+    refs.startBtn.setAttribute("disabled", true);
+    refs.stopBtn.removeAttribute("disabled");
+    timerId = setTimeout(setRandomColor, 1000);
+};
+
+function stopInterval() {
+  clearTimeout(timerId);
+  refs.startBtn.removeAttribute('disabled');
+}
